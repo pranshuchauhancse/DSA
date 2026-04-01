ea01d8a29737965c6f750b8cfcd0975d2eab8ceb
Pranshu Chauhan <pranshu121005@gmail.com>
2026-04-02 01:48:19 +0530

    A

---
+++ GeeksforGeeks/Fundamentals/Complexity Analysis/Space Complexity/A.java
@@ -0,0 +1,8 @@
+public class A {
+    int add (int n){
+    if (n <= 0){
+        return 0;
+    }
+    return n + add (n-1);
+}
+}
---
+++ GeeksforGeeks/Fundamentals/Complexity Analysis/Time Complexity/C.java
@@ -0,0 +1,23 @@
+public class C {
+    // SumOfAllElementsOfAMatrix
+    public static void main(String[] args)
+    {
+        int n = 3;
+        int m = 3;
+        int arr[][]
+            = { { 3, 2, 7 }, { 2, 6, 8 }, { 5, 1, 9 } };
+        int sum = 0;
+
+        // Iterating over all 1-D arrays in 2-D array
+        for (int i = 0; i < n; i++) {
+
+            // Printing all elements in ith 1-D array
+            for (int j = 0; j < m; j++) {
+
+                // Printing jth element of ith row
+                sum += arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
