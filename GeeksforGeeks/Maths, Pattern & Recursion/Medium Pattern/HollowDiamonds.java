2737a1c3bfd40412b484dd0024dbf4c81d4872e3
Pranshu Chauhan <pranshu121005@gmail.com>
2026-04-02 11:48:45 +0530

    A

---
+++ GeeksforGeeks/Maths, Pattern & Recursion/Medium Pattern/HollowDiamonds.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class HollowDiamonds {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        // Upper half of the diamond
+        for (int i = 1; i <= rows; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars and spaces
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        // Lower half of the diamond
+        for (int i = rows - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars and spaces
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (k == 1 || k == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
